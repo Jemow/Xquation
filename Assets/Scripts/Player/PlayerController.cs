5016a3b8fb35954719e01dbbdc0d3f42900bf5a4
Jemo <103411555+Jemow@users.noreply.github.com>
2025-12-29 16:38:36 +0100

    Implemented fire rate for player controller

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -6,19 +7,39 @@ public class PlayerController : MonoBehaviour
     [Header("References")] 
     [SerializeField] private MathProjectile projectilePrefab;
     
+    [Header("Parameters")]
+    [SerializeField] private float fireRate = 0.2f;
+    
     private MathWave _mw;
+    private Camera _mainCamera;
+
+    private bool _isFiring;
+    private bool _canFire = true;
 
     private void Start()
     {
         _mw = GetComponent<MathWave>();
+        _mainCamera = Camera.main;
+    }
+
+    private void Update()
+    {
+        if(!_isFiring || !_canFire) return;
+
+        StartCoroutine(FireCoroutine());
+        Fire();
     }
 
     public void OnAttack(InputAction.CallbackContext context)
     {
-        if(!context.started) return;
-        
+        if(context.started) _isFiring = true;
+        else if(context.canceled) _isFiring = false;
+    }
+
+    private void Fire()
+    {
         Vector3 mouseScreenPos = Mouse.current.position.ReadValue();
-        Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(mouseScreenPos);
+        Vector3 mouseWorldPos = _mainCamera.ScreenToWorldPoint(mouseScreenPos);
         mouseWorldPos.z = 0f;
 
         Vector3 dir = (mouseWorldPos - transform.position).normalized;
@@ -31,5 +52,12 @@ public class PlayerController : MonoBehaviour
             _mw.GetMaxX(), 
             _mw.GetBeamLength()
         );
+    }
+
+    private IEnumerator FireCoroutine()
+    {
+        _canFire = false;
+        yield return new WaitForSeconds(fireRate);
+        _canFire = true;
     }
 }
